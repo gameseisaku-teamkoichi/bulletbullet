dac6a20df9e2c98e02f9792b9f65303e0ce5631e
yuuma <czamy00@gmail.com>
2018-07-05 12:22:58 +0900

    変更

---
+++ Assets/script/Scene/GameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+    public ChangeScene changeScene;
+    const int NowScene = 3;
+
+    private const float TimeLimit=10.0f;
+    private float NowTime;
+
+
+	// Use this for initialization
+	void Start () {
+        NowTime = 0;
+        changeScene.Initialize(NowScene);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        NowTime += Time.deltaTime;
+
+        if(NowTime>=TimeLimit)
+            changeScene.SceneChange();
+    }
+}
