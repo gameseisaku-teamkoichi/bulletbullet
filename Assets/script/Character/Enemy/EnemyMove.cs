299fd0e88d74397657770e2fdcd417ec3caf4af8
inaly <inaly1717@gmail.com>
2018-10-12 20:08:24 +0900

    敵が弾を発射する　最新版

--- Assets/script/Character/Enemy/EnemyMove.cs
+++ Assets/script/Character/Enemy/EnemyMove.cs
@@ -12,21 +12,24 @@ public class EnemyMove : MonoBehaviour
     CharaNum charaNum;
     StageName stageName;
 
-    Vector3 position;
+    private CharacterController controller;
 
-    private float timeOut;
-    private float timeCount;
+    Vector3 position;
+    private Vector3 TraGetPosition;
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 velocity = Vector3.zero;
 
     private int directCount;
 
+    private float timeOut;
+    private float timeCount;
     private float speed = 6.0f;
     private float gravity = 20.0f;
+    private float movePower;
 
-    private Vector3 moveDirection = Vector3.zero;
-    private CharacterController controller;
-    private Vector3 velocity = Vector3.zero;
+    //敵の向きを変えるときにtrueにする
+    private bool differentMoveFlag;
 
-    private Vector3 TraGetPosition;
     private RaycastHit hit;
 
     public Vector3 Initialize(int MyNumber)
@@ -39,59 +42,58 @@ public class EnemyMove : MonoBehaviour
     public void Start()
     {
         controller = GetComponent<CharacterController>();
-        
+
         velocity.x = -0.2f;
         timeOut = 1.5f;
         timeCount = 0;
         directCount = 0;
 
+        movePower = 0.4f;
+
+        differentMoveFlag = false;
+
         //velocity +x→position.-z +z→+x -x→+z -z→-x
-       
+
     }
 
     public void enemyMove()
     {
-        timeCount += Time.deltaTime;
-
-        if (timeOut < timeCount)
+        if (differentMoveFlag)
         {
             directCount++;
             if (directCount > 3)
                 directCount = 0;
-            
-            switch (directCount)
-            {
-                //+z方向へ
-                case 0:
-                    velocity.x = -0.2f;
-                    velocity.z = 0f;
-                    break;
+        }
+        switch (directCount)
+        {
+            //+z方向へ
+            case 0:
+                velocity.x = -movePower;
+                velocity.z = 0f;
+                break;
 
-                //-x方向へ
-                case 1:
-                    velocity.x = 0f;
-                    velocity.z = -0.2f;
-                    break;
+            //-x方向へ
+            case 1:
+                velocity.x = 0f;
+                velocity.z = -movePower;
+                break;
 
-                //-z方向へ
-                case 2:
-                    velocity.x = 0.2f;
-                    velocity.z = 0f;
-                    break;
+            //-z方向へ
+            case 2:
+                velocity.x = movePower;
+                velocity.z = 0f;
+                break;
 
-                //+x方向へ
-                case 3:
-                    velocity.x = 0f;
-                    velocity.z = 0.2f;
-                    break;
-            }
-
-            timeCount = 0;
+            //+x方向へ
+            case 3:
+                velocity.x = 0f;
+                velocity.z = movePower;
+                break;
         }
 
         TraGetPosition = transform.position;
 
-        velocity = velocity.normalized * 4 * Time.deltaTime;
+        velocity = velocity.normalized * 6 * Time.deltaTime;
 
         if (velocity.magnitude > 0.1f)
         {
@@ -104,7 +106,12 @@ public class EnemyMove : MonoBehaviour
         if (Physics.Raycast(ray, out hit, 1000))
         {
             transform.position = TraGetPosition;
-            
+            differentMoveFlag = false;
+        }
+        else
+        {
+            //当たっていなければ向きを変える
+            differentMoveFlag = true;
         }
     }
 }
